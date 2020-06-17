c90da1e1a56ff255bb7f52a409720c0817a27fa9
Emily Wallace - Price <emily.wallace-price@technologywithin.com>
2020-06-17 11:25:06 +0100

    initial commit

---
+++ public/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Test Docker Setup</title>
+</head>
+<body>
+<h1>
+	Emmie's Docker
+</h1>
+<p>
+	The time is <?php echo date("Y-m-d H:i:s"); ?>
+</p>
+
+
+</body>
+</html>
